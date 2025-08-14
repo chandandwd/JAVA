0a4ada3469befcc3e5fc63cb406c440f3910fe1d
Chandan Dwivedi 0469 <chandan.dwivedi_cs24@gla.ac.in>
2025-08-14 09:31:44 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 11.11f;
+        char c = 'a';
+        String d = "Hello";
+        double x = 23.889;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(x);
+
+
+    }
+}
